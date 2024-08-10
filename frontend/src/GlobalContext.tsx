6cfb392e1d740507a4a94618ce4641994d31ff64
dacol <da.colcha@hotmail.com>
2024-08-09 23:26:35 -0500

    feat: implementación página de inicio con cambio de personaje

--- frontend/src/GlobalContext.tsx
+++ frontend/src/GlobalContext.tsx
@@ -2,20 +2,25 @@ import { createContext, useState, ReactNode, useContext } from 'react'
 
 type contextType = {
     page: string;
+    character: string;
     setPage: (value: string) => void
+    setCharacter: (value: string) => void
 }
 
 const GlobalContext = createContext<contextType | undefined>(undefined)
 
 export function GlobalContextProvider({ children }: { children: ReactNode }) {
     const [page, setPage] = useState<string>('HOME')
+    const [character, setCharacter] = useState<string>('PRINCESS')
 
     return (
-        <GlobalContext.Provider value={{ page, setPage }}>
+        <GlobalContext.Provider value={{ page, setPage, character, setCharacter }}>
             {children}
         </GlobalContext.Provider>
     )
 }
+
+
 
 export function useGlobalContext() {
     const context = useContext(GlobalContext)
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,19 +1,119 @@
-import { useGlobalContext } from "../GlobalContext"
-
-import '../styles/pages/Home.css'
+import { useGlobalContext } from "../GlobalContext";
+import logoName from "../assets/logoName.svg";
+import botonIniciar from "../assets/iniciar.svg";
+import Princess from "../assets/princess.svg";
+import Knight from "../assets/knight.svg";
+import Dino from "../assets/dino.svg";
+import btnPrincess from "../assets/princess-btn.svg";
+import btnKnight from "../assets/knight-btn.svg";
+import btnDino from "../assets/dino-btn.svg";
+import info from "../assets/info.svg";
+import "../styles/pages/Home.css";
 
 function Home() {
-  const { setPage } = useGlobalContext()
+  const { setPage, character, setCharacter } = useGlobalContext();
 
   return (
     <div className="home-page-container">
-      <div className="game-name">
-        <h1 tabIndex={0}>MATHS AND MONSTERS</h1>
-        <h5 tabIndex={0}>A game by: Guns & Coders</h5>
+      <div className="header">
+        <img
+          tabIndex={0}
+          src={logoName}
+          width="960px"
+          height="380px"
+          alt="Math Monsters"
+        />
+
+        <div className="character-info">
+          {character === "PRINCESS" && (
+            <img
+              tabIndex={0}
+              role="button"
+              src={btnPrincess}
+              width="194px"
+              height="164px"
+              onClick={() => setCharacter("KNIGHT")}
+              className="select-character"
+              alt="Seleccionar personaje"
+            />
+          )}
+          {character === "KNIGHT" && (
+            <img
+              tabIndex={0}
+              role="button"
+              src={btnKnight}
+              width="194px"
+              height="164px"
+              onClick={() => setCharacter("DINO")}
+              className="select-character"
+              alt="Seleccionar personaje"
+            />
+          )}
+          {character === "DINO" && (
+            <img
+              tabIndex={0}
+              role="button"
+              src={btnDino}
+              width="194px"
+              height="164px"
+              onClick={() => setCharacter("PRINCESS")}
+              className="select-character"
+              alt="Seleccionar personaje"
+            />
+          )}
+
+          <img
+            tabIndex={0}
+            role="button"
+            src={info}
+            width="44px"
+            height="44px"
+            className="info"
+            alt="Información"
+          />
+        </div>
       </div>
-      <button tabIndex={0} onClick={() => setPage('GAME')}>PLAY</button>
+      <div className="start">
+        <img
+          tabIndex={0}
+          role="button"
+          onClick={() => setPage("GAME")}
+          alt="Iniciar juego"
+          src={botonIniciar}
+          width="505px"
+          height="127px"
+        />
+
+        {character === "PRINCESS" && (
+          <img
+            tabIndex={0}
+            src={Princess}
+            width="227px"
+            height="320px"
+            alt="Princesa"
+          />
+        )}
+        {character === "KNIGHT" && (
+          <img
+            tabIndex={0}
+            src={Knight}
+            width="227px"
+            height="320px"
+            alt="Príncipe"
+          />
+        )}
+        {character === "DINO" && (
+          <img
+            tabIndex={0}
+            src={Dino}
+            width="227px"
+            height="320px"
+            alt="Dinosaurio"
+          />
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
